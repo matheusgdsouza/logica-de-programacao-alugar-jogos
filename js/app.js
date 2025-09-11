214d4f3e5a3a87862ea52de7afb82b3cef050e21
Matheus de Souza <matheusgds3011@gmail.com>
2025-09-11 00:28:12 -0300

    Adicionando a lógica para alteração do elemento e ajustando códigos para obter os elementos de forma correta

--- js/app.js
+++ js/app.js
@@ -1,7 +1,17 @@
 function alterarStatus(n){
     let jogoSelecionado = document.getElementById(`game-${n}`);
-    let imagemJogo = jogoSelecionado.classList(".dashboard__item__img");
-    let botaoJogo = jogoSelecionado.classList(".dashboard__item__button");
+    let imagemJogo = jogoSelecionado.querySelector(".dashboard__item__img");
+    let botaoJogo = jogoSelecionado.querySelector(".dashboard__item__button");
+
+    if(imagemJogo.classList.contains("dashboard__item__img--rented")){
+        imagemJogo.classList.remove("dashboard__item__img--rented");
+        botaoJogo.classList.remove("dashboard__item__button--return");
+        botaoJogo.textContent = "Alugar";
+    } else {
+        imagemJogo.classList.add("dashboard__item__img--rented");
+        botaoJogo.classList.add("dashboard__item__button--return");
+        botaoJogo.textContent = "Devolver";
+    }
 }
 
 
