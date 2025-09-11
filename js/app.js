acd09704504dee40558d3c9df126d6ab41999ff8
Matheus de Souza <matheusgds3011@gmail.com>
2025-09-11 00:00:14 -0300

    Pegando valores dos elementos necessários para as alterações

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
-
+function alterarStatus(n){
+    let jogoSelecionado = document.getElementById(`game-${n}`);
+    let imagemJogo = jogoSelecionado.classList(".dashboard__item__img");
+    let botaoJogo = jogoSelecionado.classList(".dashboard__item__button");
+}
 
 
